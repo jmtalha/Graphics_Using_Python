6f4aefa0dd5bf30161e4e71f4301385c65c91517
JMTalha <92794786+jmtalha@users.noreply.github.com>
2021-10-19 22:40:11 +0530

    08_Graphics

---
+++ 08_Graphics.py
@@ -0,0 +1,16 @@
+import time
+import turtle as t
+from turtle import *
+
+bgcolor("black")
+speed(12)
+
+for i in range(50):
+    pensize(1)
+    pencolor("white")
+    circle(100)
+    forward(5)
+    right(25)
+
+t.done()
+time.sleep(5)
