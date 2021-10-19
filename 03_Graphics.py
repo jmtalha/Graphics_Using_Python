567494ba0813b6f3938bf2227a2580d55a3769e2
JMTalha <92794786+jmtalha@users.noreply.github.com>
2021-10-19 22:34:47 +0530

    03_Graphics

---
+++ 03_Graphics.py
@@ -0,0 +1,15 @@
+import time
+import turtle
+import random
+screen = turtle.Screen()
+bob = turtle.Turtle()
+screen.bgcolor('black')
+bob.pencolor('silver')
+bob.speed(100)
+def crazy(var1):
+    for i in range(200):
+        bob.forward(i)
+        bob.left(var1)
+
+crazy(300)
+time.sleep(5)
